f4392c0420670f0a42f0e5e7d51a95b0e8612c87
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2025-03-26 22:57:28 +0900

    Refactor: Enable mocking in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ async function enableMocking() {
   // if (process.env.NODE_ENV !== "development") {
   //   return;
   // }
-  // const { worker } = await import("./mocks/browser");
-  // return worker.start();
+  const { worker } = await import("./mocks/browser");
+  return worker.start();
 }
 
 enableMocking().then(() => {
